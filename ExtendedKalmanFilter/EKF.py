3c709b14512c7be26901a2dfe3f2c0c46c93e703
T0NY1302 <98052680+T0NY1302@users.noreply.github.com>
2022-06-09 17:57:30 +0100

    Pendulum EKF implementation

--- ExtendedKalmanFilter/EKF.py
+++ ExtendedKalmanFilter/EKF.py
@@ -10,34 +10,11 @@ def predict(f, F, Q, m_t, P_t):
 def update(h, H, R, y, predicted_m, predicted_P):
     v = y - h(predicted_m)
     S = H(predicted_m) @ predicted_P @ np.transpose(H(predicted_m)) + R
-    if S.ndim == 1:
+    if S[0].ndim == 1:
         S_inv = np.array([1/S[0]])
     else:
         S_inv = np.linalg.inv(S)
-    
-    #Below since tranpose of 1D array 
-    if (predicted_P @ np.transpose(H(predicted_m))).ndim == 1:
-        K_res_1 = (predicted_P @ np.transpose(H(predicted_m)))[np.newaxis]
-        K_res_1 = K_res_1.T
-    else:
-        K_res_1 = predicted_P @ np.transpose(H(predicted_m))
-
-    K = K_res_1 @ S_inv
-
-    if (predicted_m + K).ndim == 1:
-        updated_m_res_1 = (predicted_m + K)[np.newaxis]
-        updated_m_res_1 = updated_m_res_1.T
-    else:
-        updated_m_res_1 = predicted_m + K
-    updated_m = updated_m_res_1 @ v
-
-    if K.ndim == 1:
-        updated_res_1 = K[np.newaxis].T
-        t = K[np.newaxis].T @ S
-        res = t[np.newaxis].T
-        updated_res_2 = res @ K[np.newaxis]
-    else:
-        updated_res_2 = K @ S @ np.transpose(K)
-
-    updated_P = predicted_P - updated_res_2
+    K = predicted_P @ np.transpose(H(predicted_m)) @ S_inv
+    updated_m = predicted_m + K @ v
+    updated_P = predicted_P - K @ S @ np.transpose(K)
     return updated_m, updated_P
--- ExtendedKalmanFilter/pendulum_demo.py
+++ ExtendedKalmanFilter/pendulum_demo.py
@@ -10,34 +10,33 @@ g = 9.8
 sigma = 0.1
 
 def f(x):
-    return np.transpose(np.array([x[0] + x[1]*dt,
-                    x[1] - g * np.sin(x[0]) * dt]))
+    return np.transpose(np.array([[x[0, 0] + x[1, 0]*dt,
+                    x[1, 0] - g * np.sin(x[0, 0]) * dt]]))
 
 def F(x):
     return np.array([[1, dt],
-                     [-1 * g * np.cos(x[0]) * dt, 1]])
+                     [-1 * g * np.cos(x[0, 0]) * dt, 1]])
 
 def H(x):
-    return np.array([np.cos(x[0]), 0])
+    return np.array([[np.cos(x[0, 0]), 0]])
 
 def h(x):
-    return np.array([np.sin(x[0])])
+    return np.array([[np.sin(x[0, 0])]])
 
 
 Q = np.array([[(q_c * dt ** 3) / 3, (q_c * dt ** 2) / 2],
              [(q_c * dt ** 2) / 2, q_c * dt]]) 
 
-R = np.array([sigma**2])
-
+R = np.array([[sigma**2]])
 
 
 #Simulate Pendulum
 num_steps = 200
 alpha, v = 1.5, 0
-true_states = [np.array([alpha, v])]
+true_states = [np.array([[alpha, v]]).T]
 for _ in range(num_steps):
     motion_noise = np.random.multivariate_normal(mean=np.array([0,0]), cov=Q)
-    new_state = f(true_states[-1]) + motion_noise
+    new_state = f(true_states[-1]) + np.array([motion_noise]).T
     true_states.append(new_state)
 
 #Simulate Measurement
@@ -51,7 +50,8 @@ true_states = np.array(true_states)
 new_angle = np.array(new_angle)
 
 filtered_states = []
-m_0 = np.array([0, 0])
+
+m_0 = np.array([[1.5, 0]]).T
 P_0 = np.zeros((2, 2))
 m_current = m_0.copy()
 P_current = P_0.copy()
