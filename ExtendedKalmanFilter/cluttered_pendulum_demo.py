5d963ca849e4d296af74da51feccc7aab08d5f9f
Tony Dam <tv220@ic.ac.uk>
2022-06-11 12:33:40 +0100

    EKF cluttered pendulum

---
+++ ExtendedKalmanFilter/cluttered_pendulum_demo.py
@@ -0,0 +1,74 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from EKF import predict, update
+
+
+#Pendulum tracking with EKF (x 2d)
+dt = 0.05
+q_c = 0.1
+g = 9.8
+sigma = 0.1
+
+def f(x):
+    return np.transpose(np.array([[x[0, 0] + x[1, 0]*dt,
+                    x[1, 0] - g * np.sin(x[0, 0]) * dt]]))
+
+def F(x):
+    return np.array([[1, dt],
+                     [-1 * g * np.cos(x[0, 0]) * dt, 1]])
+
+def H(x):
+    return np.array([[np.cos(x[0, 0]), 0]])
+
+def h(x):
+    return np.array([[np.sin(x[0, 0])]])
+
+
+Q = np.array([[(q_c * dt ** 3) / 3, (q_c * dt ** 2) / 2],
+             [(q_c * dt ** 2) / 2, q_c * dt]]) 
+
+R = np.array([[sigma**2]])
+
+
+#Simulate Pendulum
+num_steps = 200
+alpha, v = 1.5, 0
+true_states = [np.array([[alpha, v]]).T]
+for _ in range(num_steps):
+    motion_noise = np.random.multivariate_normal(mean=np.array([0,0]), cov=Q)
+    new_state = f(true_states[-1]) + np.array([motion_noise]).T
+    true_states.append(new_state)
+
+#Simulate Measurement
+measurement_angle = [np.array([alpha])]
+for i in range(num_steps):
+    measurement_noise = np.random.normal(0, sigma)
+    new_angle1 = h(true_states[i]) + measurement_noise
+    new_angle2 = np.random.uniform(-alpha, alpha)
+    new_angle = np.random.choice([new_angle1, new_angle2])
+    measurement_angle.append(new_angle)
+
+true_states = np.array(true_states)
+new_angle = np.array(new_angle)
+
+filtered_states = []
+
+m_0 = np.array([[1.5, 0]]).T
+P_0 = np.zeros((2, 2))
+m_current = m_0.copy()
+P_current = P_0.copy()
+
+for i in range(num_steps):
+    predicted_m, predicted_P = predict(f, F, Q, m_current, P_current)
+    y = measurement_angle[i+1]
+    m_current, P_current = update(h, H, R, y, predicted_m, predicted_P)
+    filtered_states.append(m_current)
+
+filtered_states = np.array(filtered_states)
+print(len(filtered_states), len(measurement_angle))
+
+t = np.arange(0, dt*num_steps + dt, dt)
+plt.plot(t, np.sin(true_states[:, 0]))
+plt.scatter(t, measurement_angle)
+plt.scatter(t[1:], np.sin(filtered_states[:, 0]))
+plt.show()
